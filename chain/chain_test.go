52cfa1415e61c6dfbf92f9c17a89e773ef5546a9
Katrina Owen <katrina.owen@gmail.com>
2014-04-29 06:08:01 -0400

    Make table test extensible

--- chain/chain_test.go
+++ chain/chain_test.go
@@ -6,33 +6,42 @@ import (
 	"testing"
 )
 
-var chainTests = []Node{
-	{" ", []string{"I"}},
-	{" I", []string{"am"}},
-	{"I am", []string{"a", "not"}},
-	{"a free", []string{"man."}},
-	{"free man.", []string{"I"}},
-	{"man. I", []string{"am"}},
-	{"not a", []string{"number."}},
-	{"am a", []string{"free"}},
-	{"am not", []string{"a"}},
+var chainTests = []struct {
+	input string
+	nodes []Node
+}{
+	{
+		"I am a free man. I am not a number.",
+		[]Node{
+			{" ", []string{"I"}},
+			{" I", []string{"am"}},
+			{"I am", []string{"a", "not"}},
+			{"a free", []string{"man."}},
+			{"free man.", []string{"I"}},
+			{"man. I", []string{"am"}},
+			{"not a", []string{"number."}},
+			{"am a", []string{"free"}},
+			{"am not", []string{"a"}},
+		},
+	},
 }
 
 func TestChain(t *testing.T) {
-	c := NewChain(2)
-	s := "I am a free man. I am not a number."
-	br := bytes.NewBufferString(s)
-	c.Build(br)
-
-	if len(chainTests) != len(c.Nodes) {
-		t.Fatalf("The chain is the wrong size. Expected: %v, Got: %v", len(chainTests), len(c.Nodes))
-	}
-
 	for _, tt := range chainTests {
-		node := c.FindNode(tt.Key)
+		c := NewChain(2)
+		br := bytes.NewBufferString(tt.input)
+		c.Build(br)
 
-		if !reflect.DeepEqual(tt.Fragments, node.Fragments) {
-			t.Fatalf("%v != %v", tt.Fragments, node.Fragments)
+		if len(tt.nodes) != len(c.Nodes) {
+			t.Fatalf("The chain is the wrong size. Expected: %v, Got: %v", len(tt.nodes), len(c.Nodes))
+		}
+
+		for _, node := range tt.nodes {
+			fragments := c.FindNode(node.Key).Fragments
+
+			if !reflect.DeepEqual(node.Fragments, fragments) {
+				t.Fatalf("%v != %v", node.Fragments, fragments)
+			}
 		}
 	}
 }
