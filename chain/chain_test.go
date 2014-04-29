344805e7eebcf2ca45e231e548e19b99b4f95432
Katrina Owen <katrina.owen@gmail.com>
2014-04-28 22:08:31 -0400

    Clean up chain tests

--- chain/chain_test.go
+++ chain/chain_test.go
@@ -3,38 +3,36 @@ package chain
 import (
 	"bytes"
 	"reflect"
-	"sort"
 	"testing"
 )
 
-type ByKey []*Node
-
-func (n ByKey) Len() int           { return len(n) }
-func (n ByKey) Less(i, j int) bool { return n[i].Key < n[j].Key }
-func (n ByKey) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+var chainTests = []Node{
+	{" ", []string{"I"}},
+	{" I", []string{"am"}},
+	{"I am", []string{"a", "not"}},
+	{"a free", []string{"man."}},
+	{"free man.", []string{"I"}},
+	{"man. I", []string{"am"}},
+	{"not a", []string{"number."}},
+	{"am a", []string{"free"}},
+	{"am not", []string{"a"}},
+}
 
 func TestChain(t *testing.T) {
 	c := NewChain(2)
 	s := "I am a free man. I am not a number."
 	br := bytes.NewBufferString(s)
-	expected := []*Node{
-		{" ", []string{"I"}},
-		{" I", []string{"am"}},
-		{"I am", []string{"a", "not"}},
-		{"a free", []string{"man."}},
-		{"free man.", []string{"I"}},
-		{"man. I", []string{"am"}},
-		{"not a", []string{"number."}},
-		{"am a", []string{"free"}},
-		{"am not", []string{"a"}},
-	}
-
 	c.Build(br)
 
-	sort.Sort(ByKey(expected))
-	sort.Sort(ByKey(c.Nodes))
+	if len(chainTests) != len(c.Nodes) {
+		t.Fatalf("The chain is the wrong size. Expected: %v, Got: %v", len(chainTests), len(c.Nodes))
+	}
 
-	if !reflect.DeepEqual(expected, c.Nodes) {
-		t.Fatalf("%v != %v", expected, c.Nodes)
+	for _, tt := range chainTests {
+		node := c.FindNode(tt.Key)
+
+		if !reflect.DeepEqual(tt.Fragments, node.Fragments) {
+			t.Fatalf("%v != %v", tt.Fragments, node.Fragments)
+		}
 	}
 }
